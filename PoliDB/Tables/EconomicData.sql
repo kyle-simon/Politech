65e08501b43ec15ac56c5068d1f21675ef125fd2
Kyle Simon <Kyle.Simon@stonybrook.edu>
2019-03-12 13:59:51 -0400

    Refactored Population table into Demographic table and EconomicData table

---
+++ PoliDB/Tables/EconomicData.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[EconomicData]
+(
+	[Id] INT NOT NULL PRIMARY KEY NONCLUSTERED, 
+    [GDPPerCapita] MONEY NULL, 
+    [MedianIncome] NCHAR(10) NULL, 
+    [PrecinctId] INT NOT NULL, 
+    [Year] DATE NOT NULL, 
+    CONSTRAINT [FK_EconomicData_Precinct] FOREIGN KEY ([PrecinctId]) REFERENCES [Precinct]([Id])
+)
