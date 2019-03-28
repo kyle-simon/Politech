00f189e131b8b9344d3a03f6a5fa885db393ac10
Kyle Simon <Kyle.Simon@stonybrook.edu>
2019-03-28 09:18:00 -0400

    Added remaining missing models

--- PoliTech/api/models.py
+++ PoliTech/api/models.py
@@ -12,6 +12,9 @@ class Precinct(models.Model):
     description = models.CharField(max_length=200)
     adjacencies = models.ManyToManyField('self', through='Adjacency', symmetrical=False, related_name='related_to+')
     # The + after related_to is required, it makes it so Django will not expose the backwards relationship
+    # While the adjacency relation is symmetric, Django doesn't support symmetric relations with a through table, so we have to add some
+    # helper methods to make it behave like a symmetric relationship
+
     def add_adjacency(self, precinct, sym = True):
         adjacency, created = Adjacency.objects.get_or_create(from_precinct=self, to_precinct=precinct)
 
@@ -52,4 +55,68 @@ class Adjacency(models.Model):
 
 class AdjacencyType(models.Model):
     description = models.CharField(max_length=200)
+
+class Demographic(models.Model):
+    contains_representative = models.BooleanField(null=True, blank=True)
+    year = models.DateField()
+    total_population = models.IntegerField(null=True, blank=True)
+    precinct = models.ForeignKey(Precinct)
+    demographic_types = models.ManyToManyField(DemographicType, through='DemographicTypePopulation')
+
+    class Meta:
+        index_together = [
+            ('precinct', 'year')    
+        ]
+
+class DemographicTypePopulation:
+    demographic = models.ForeignKey(Demographic)
+    demographic_type = models.ForeignKey(DemographicType)
+    population = models.IntegerType()
+
+    class Meta:
+        index_together =[
+            ('demographic', 'demographic_type')    
+        ]
+
+class DemographicType(models.Model):
+    description = models.CharField(max_length=200)
+
+class EconomicData(models.Model):
+    gdp_per_capita = models.DecimalField(max_digits=15, decimal_places=2, null=True, blank=True)
+    median_income = models.DecimalField(max_digits=15, decimal_places=2, null=True, blank=True)
+    year = models.DateField()
+    precinct = models.ForeignKey(Precinct)
+
+class ElectionResult(models.Model):
+    election_year = models.Date()
+    precinct = models.ForeignKey(Precinct)
+    votes = models.ManyToManyField(PoliticalParties, through='VoteCount')
+
+    class Meta:
+        index_together = [
+            ('precinct', 'election_year')    
+        ]
+
+class VoteCount(models.Model):
+    election_result = models.ForeignKey(ElectionResult)
+    political_party = models.ForeignKey(PoliticalParty)
+    num_votes = models.IntegerField()
+
+class PoliticalParty(models.Model):
+    description = models.CharField(max_length=100)
     
+class District(models.Model):
+    state = models.CharField(max_length=2)
+    description = models.CharField(max_length=200)
+    precincts = models.ManyToManyField(Precinct, through='DistrictMembership')
+
+class DistrictMembership(models.Model):
+    precinct = models.ForeignKey(Precinct)
+    district = models.ForeignKey(District)
+    from_year = models.Date()
+    to_year = models.Date(null=True, blank=True)
+
+    class Meta:
+        index_together = [
+            ('district', 'precinct', 'from_year')    
+        ]
