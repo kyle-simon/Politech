f2d632e498666c493d9a9de0e6b2b0aebfb8d5ae
Bingling Dong <bingling.dong@stonybrook.edu>
2019-04-07 15:00:39 -0400

    Sample put function created in apps.py.

--- PoliTech/api/views.py
+++ PoliTech/api/views.py
@@ -1,5 +1,35 @@
 from django.shortcuts import render
-from api.apps import  *
+from rest_framework import generics
+from .models import District
+from .serializers import DistrictSerializer
+from django.http import JsonResponse
+
+
 # Create your views here.
 # def index(request):
-#     data =
+
+class districtCreateView(generics.ListCreateAPIVIEW):
+    queryset = District.objects.all()
+    serializer_class = DistrictSerializer
+
+    def create(self, request):
+        write_serializer = DistrictSerializer(data=request.data)
+        write_serializer.is_valid(raise_exception=True)
+        instance = self.perform_create(write_serializer)
+
+        read_serializer = DistrictSerializer(instance)
+
+        return JsonResponse(read_serializer.data)
+
+
+
+
+
+
+
+
+
+
+
+
+
