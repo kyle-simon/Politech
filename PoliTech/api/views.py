baaa0c928a44f417e327f92030e3a0444ac0f5d6
AjaySarjooCSE219 <ajay.sarjoo@stonybrook.edu>
2019-04-07 15:30:50 -0400

    added views for all serializers, only using economic and voting count for now

--- PoliTech/api/views.py
+++ PoliTech/api/views.py
@@ -17,6 +17,71 @@ class EconomicListCreateView(generics.ListCreateAPIView):
         read_serializer = EconomicDataSerializer(instance)
         return JsonResponse(read_serializer.data)
 
+# class DistrictCreateView(generics.ListCreateAPIView):
+#     queryset = District.objects.all()
+#     serializer_class = DistrictSerializer
+#
+#     def create(self, request, *args, **kwargs):
+#         write_serializer = DistrictSerializer(data=request.data)
+#         write_serializer.is_valid(raise_exception=True)
+#         instance = self.perform_create(write_serializer)
+#         read_serializer = DistrictSerializer(instance)
+#         return JsonResponse(read_serializer.data)
+#
+# class AdjacencyTypeCreateView(generics.ListCreateAPIView):
+#     queryset = AdjacencyType.objects.all()
+#     serializer_class = AdjacencyType
+#
+#     def create(self, request, *args, **kwargs):
+#         write_serializer = AdjacencyTypeSerializer(data=request.data)
+#         write_serializer.is_valid(raise_exception=True)
+#         instance = self.perform_create(write_serializer)
+#         read_serializer = AdjacencyTypeSerializer(instance)
+#         return JsonResponse(read_serializer.data)
+#
+# class DemographicTypeCreateView(generics.ListCreateAPIView):
+#     queryset = DemographicType.objects.all()
+#     serializer_class = DemographicType
+#
+#     def create(self, request, *args, **kwargs):
+#         write_serializer = DemographicTypeSerializer(data=request.data)
+#         write_serializer.is_valid(raise_exception=True)
+#         instance = self.perform_create(write_serializer)
+#         read_serializer = DemographicTypeSerializer(instance)
+#         return JsonResponse(read_serializer.data)
+#
+# class DemographicCreateView(generics.ListCreateAPIView):
+#     queryset = Demographic.objects.all()
+#     serializer_class = Demographic
+#
+#     def create(self, request, *args, **kwargs):
+#         write_serializer = DemographicSerializer(data=request.data)
+#         write_serializer.is_valid(raise_exception=True)
+#         instance = self.perform_create(write_serializer)
+#         read_serializer = DemographicSerializer(instance)
+#         return JsonResponse(read_serializer.data)
+#
+# class AdjacencyCreateView(generics.ListCreateAPIView):
+#     queryset = Adjacency.objects.all()
+#     serializer_class = Adjacency
+#
+#     def create(self, request, *args, **kwargs):
+#         write_serializer = AdjacencySerializer(data=request.data)
+#         write_serializer.is_valid(raise_exception=True)
+#         instance = self.perform_create(write_serializer)
+#         read_serializer = AdjacencySerializer(instance)
+#         return JsonResponse(read_serializer.data)
+#
+# class PrecinctCreateView(generics.ListCreateAPIView):
+#     queryset = Precinct.objects.all()
+#     serializer_class = Precinct
+#
+#     def create(self, request, *args, **kwargs):
+#         write_serializer = PrecinctSerializer(data=request.data)
+#         write_serializer.is_valid(raise_exception=True)
+#         instance = self.perform_create(write_serializer)
+#         read_serializer = PrecinctSerializer(instance)
+
 class VoteCountCreateView(generics.ListCreateAPIVIEW):
     queryset = VoteCount.objects.all()
     serializer_class = VoteCountSerializer
